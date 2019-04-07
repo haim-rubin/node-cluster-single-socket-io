24ec63d09ce0ae23c1e3b0b445e45953eca5b2ac
Haim Rubin <haimr@nsogroup.com>
2019-04-07 17:49:03 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# node-cluster-single-socket-io
+This repository is for learning. DO NOT USE IN PRODUCTION!!!!!!!!!!!
---
+++ index.js
@@ -0,0 +1,46 @@
+var cluster = require('cluster');
+var os = require('os');
+
+if (cluster.isMaster) {
+  // we create a HTTP server, but we do not use listen
+  // that way, we have a socket.io server that doesn't accept connections
+  var server = require('http').createServer();
+  var io = require('socket.io').listen(server);
+  var redis = require('socket.io-redis');
+
+  io.adapter(redis({ host: 'localhost', port: 6379 }));
+
+  setInterval(function() {
+    // all workers will receive this in Redis, and emit
+    io.emit('data', 'payload');
+  }, 1000);
+
+  for (var i = 0; i < os.cpus().length; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', function(worker, code, signal) {
+    console.log('worker ' + worker.process.pid + ' died');
+  });
+}
+
+if (cluster.isWorker) {
+  var express = require('express');
+  var app = express();
+
+  var http = require('http');
+  var server = http.createServer(app);
+  var io = require('socket.io').listen(server);
+  var redis = require('socket.io-redis');
+app.get('/haim', (req, res) =>{
+    res.json({data: 'RUBIN'})
+})
+ // io.adapter(redis({ host: 'localhost', port: 6379 }));
+  io.on('connection', function(socket) {
+    socket.emit('data', 'connected to worker: ' + process.pid);
+  });
+
+  app.listen(5555, () =>{
+      console.log(`${process.pid} - Server is up...`)
+  });
+}
